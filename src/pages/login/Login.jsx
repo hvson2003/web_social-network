7f39824dd228c27f5a1b051f78d8807dc5f4e132
hvson03 <hvson2003@gmail.com>
2024-10-16 12:50:38 +0700

    Finish register page design and fix layout patterns in login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,9 +5,9 @@ const Login = () => {
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Hello World.</h1>
+          <h1>Social Media</h1>
           <p>
-            This is just a temp comment
+            Welcome to our social media, where you can connect, share, and engage with friends and communities. Join us and be part of a vibrant digital space!
           </p>
           <span>Don't you have an account?</span>
           <button>Register</button>
