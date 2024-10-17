69a7805b37cc5f842949fa1a6677fed30ff90f9b
hvson03 <hvson2003@gmail.com>
2024-10-17 10:52:00 +0700

    Complete comments design in posts and user profile layout

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,8 +5,12 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Comments from "../comments/Comments";
 
 const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
+
   const liked = false;
 
   return (
@@ -24,16 +28,18 @@ const Post = ({ post }) => {
           </div>
           <MoreHorizIcon />
         </div>
+
         <div className="content">
           <p>{post.desc}</p>
           <img src={post.img} alt="" />
         </div>
+        
         <div className="info">
           <div className="item">
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
             12 Likes
           </div>
-          <div className="item">
+          <div className="item" onClick={()=>setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
             20 Comments
           </div>
@@ -42,6 +48,7 @@ const Post = ({ post }) => {
             Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div >
   )
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,60 @@
+import { EmailOutlined, FacebookTwoTone, Instagram, Language, LinkedIn, MoreVert, Pinterest, Place, Twitter } from '@mui/icons-material'
 import './profile.scss'
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
   return (
-    <div className="profile">Profile</div>
+    <div className="profile">
+      <div className="images">
+        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="cover" />
+        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" className="profilePic" />
+      </div>
+
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoTone fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Instagram fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Twitter fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedIn fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Pinterest fontSize="large" />
+            </a>
+          </div>
+
+          <div className="center">
+            <span>Son Hoang</span>
+            <div className="info">
+              <div className="item">
+                <Place />
+                <span>Vietnam</span>
+              </div>
+
+              <div className="item">
+                <Language />
+                <span>hvson.dev</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+
+          <div className="right">
+            <EmailOutlined />
+            <MoreVert />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
   )
 }
 
-export default Profile
+export default Profile;
